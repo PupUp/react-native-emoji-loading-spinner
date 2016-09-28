b2691358e5e54f760cf7e45c02e1fe62dbd7dbf4
Arjun Balaji <arjunblj@gmail.com>
2016-09-28 16:11:18 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Text, StyleSheet, Animated, View, Easing } from 'react-native'
+import nodeEmoji from 'node-emoji'
+
+class EmojiSpinner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      angle: new Animated.Value(0),
+    }
+  }
+
+  static propTypes = {
+    emojiName: React.PropTypes.string.isRequired,
+  }
+
+  componentDidMount() {
+    this._animate()
+  }
+
+  _animate() {
+    this.state.angle.setValue(0)
+    this._anim = Animated.timing(this.state.angle, {
+      toValue: 360 * 400,
+      duration: 720 * 400,
+      easing: Easing.linear
+    }).start(this._animate)
+  }
+
+  getRotationAnimation() {
+    return [styles.rotateCard,
+      {transform: [
+        {rotate: this.state.angle.interpolate({
+          inputRange: [0, 360],
+          outputRange: ['0deg', '360deg']
+        })},
+      ]}
+    ]
+  }
+
+  render() {
+    const { emojiName, emojiSize } = this.props
+
+    const emoji = nodeEmoji.get(emojiName)
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Animated.View style={this.getRotationAnimation()}>
+          <Text style={{fontSize: emojiSize}}>{ emoji }</Text>
+        </Animated.View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  rotateCard: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+  },
+})
+
+export default EmojiSpinner
